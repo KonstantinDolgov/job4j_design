6fe80d1bee7d7fe2d2f5dbe4de3022882bbf2e66
KonstantinDolgov <dolgovkos777@gmail.com>
2023-03-01 20:50:03 +0300

    2.0. FileOutputStream [#252488 #396218]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,25 +1,19 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.util.StringJoiner;
+import java.io.IOException;
 
 public class ResultFile {
-    public static String multiple(int size) {
-        StringJoiner joiner = new StringJoiner(" ");
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
-                int rsl = (row + 1) * (cell + 1);
-                joiner.add(String.valueOf(rsl));
-            }
-        }
-        return joiner.toString();
-    }
-
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            out.write((multiple(3)).getBytes());
-            out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+            for (int row = 2; row < 10; row++) {
+                for (int cell = 1; cell <= 10; cell++) {
+                    out.write(String.valueOf(row * cell).getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
