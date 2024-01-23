f5aad1f53eaa3e16eb0c7526902afbae621a49db
KonstantinDolgov <dolgovkos777@gmail.com>
2024-01-23 21:18:55 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

--- db/machine_tools.sql
+++ db/machine_tools.sql
@@ -24,3 +24,37 @@ select sum(year) from tools;
 update tools set year = 50 where model = '6T13';
 
 commit;
+
+insert into tools (model, year, type) VALUES ('SMV', 12, 'miller');
+
+commit transaction;
+
+select * from tools;
+
+begin transaction;
+
+delete from tools;
+
+drop table tools;
+
+rollback transaction;
+
+select * from tools;
+
+begin transaction;
+
+insert into tools (model, year, type) VALUES ('16A20', 2, 'lathe');
+
+savepoint first_savepoint;
+
+delete from tools where type = 'miller';
+
+update tools set year = 0 where model = '1K62';
+
+select * from tools;
+
+rollback to first_savepoint;
+
+select * from tools;
+
+commit transaction;
